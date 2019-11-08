9ed448259f0b1e5686a0d68d165141b195254316
Mário Gil <mg_mesquita@hotmail.com>
2019-11-08 18:24:49 +0000

    ActionCreators and Middleware

--- guideasy_app/lib/redux/Actions.dart
+++ guideasy_app/lib/redux/Actions.dart
@@ -1,7 +1,8 @@
 import 'package:guideasy_app/model/AppState.dart';
+import 'package:guideasy_app/model/PointOfInterest.dart';
 
 class SavePOIsAction {
-  List<dynamic> pointsOfInterest;
+  List<PointOfInterest> pointsOfInterest;
   SavePOIsAction(this.pointsOfInterest);
 }
 
